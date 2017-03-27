847f945c2614683c4e4a89103b2a8d4159852488
aditya <aadityajajodia@gmail.com>
2017-03-27 23:41:06 +0530

    FIFO page replacement

---
+++ lab9/Q1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int fnum;
+int pnum;
+int parr[20];
+int farr[5];
+int count=-1;
+int i,j;
+int fault=0;
+void main()
+{
+	printf("Enter number of frames\n");
+	scanf("%d",&fnum);
+	printf("Enter number of pages\n");
+	scanf("%d",&pnum);
+	for(i=0;i<pnum;i++)
+	{
+		printf("Enter page number\n");
+		scanf("%d",&parr[i]);
+	}
+	for(i=0;i<fnum;i++)
+		farr[i]=-1;
+	for(i=0;i<pnum;i++)
+	{
+		int temp=parr[i];
+		int flag=0;
+		for(j=0;j<fnum;j++)
+			if(temp==farr[j])
+			{	
+				flag=1;
+				break;
+			}
+		if(flag==0)
+		{
+			fault++;
+			count=(count+1)%fnum;
+			farr[count]=temp;
+		}
+		printf("%d %d %d\n",farr[0],farr[1],farr[2] );
+	}
+	printf("Number of fault = %d\n",fault );
+}
